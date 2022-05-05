c6b1c92e2227e2e120d13a4161b1269d6c4c85b3
uhey22e <uhey22e@users.noreply.github.com>
2022-05-06 03:14:50 +0900

    Add Amazon SES client

---
+++ amazon_ses_test.go
@@ -0,0 +1,21 @@
+package hedwig
+
+import (
+	"context"
+	"testing"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+)
+
+func TestAmazonSESClient_SendMail(t *testing.T) {
+	// Test run without error
+	ctx := context.TODO()
+	cfg, err := config.LoadDefaultConfig(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	c := NewAmazonSESClient(cfg)
+	if err := c.SendMail(ctx, content); err != nil {
+		t.Errorf("AmazonSESClient.SendMail() error = %v", err)
+	}
+}
